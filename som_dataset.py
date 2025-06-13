66908cfa24de1056b7998965b7aa67d62e403142
Arif <arif.rahman@wcma.vic.gov.au>
2025-06-13 16:13:22 +1000

    dione

--- som_dataset.py
+++ som_dataset.py
@@ -1,18 +1,23 @@
 import pandas as pd
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
 
 class SomDataset(Dataset):
     def __init__(self, is_train=True):
-        df = pd.read_csv("data/exported/vectis_full.csv")
-        df = df.dropna()
+        df = pd.read_csv("data/exported/vectis_full.csv").dropna()
+
         bands = ["B2", "B3", "B4", "B5", "B6", "B7", "B8", "B8A", "B11", "B12"]
         X = df[bands].values.astype("float32")
         y = df["som"].values.astype("float32")
 
+        scaler = MinMaxScaler()
+        X_scaled = scaler.fit_transform(X)
+
         X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.25, random_state=42
+            X_scaled, y, test_size=0.25, random_state=42
         )
 
         self.X = torch.tensor(X_train if is_train else X_test)
